0148c4af7d235d5b7d46106b24eab4fd0505e107
Ankit Sharma <107275496+ANkZz1997@users.noreply.github.com>
2025-03-29 16:21:28 +0530

    Delete app directory

--- app/pages/setting/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div className='backdrop-blur-sm h-full w-full'>page</div>
-  )
-}
--- app/utils/global.d.ts
+++
@@ -1,7 +0,0 @@
-// global.d.ts
-import { MongoClient } from 'mongodb';
-
-declare global {
-  // Declaring the global interface to have _mongoClientPromise of type Promise<MongoClient>
-  var _mongoClientPromise: Promise<MongoClient>;
-}
